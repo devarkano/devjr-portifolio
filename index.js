9aba8453942270000f9de0043df8d81ded658a44
junior <95625738+devarkano@users.noreply.github.com>
2023-10-07 14:21:32 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const projectElements = document.querySelectorAll('.project');
 
+projectElements.forEach((project) => {
+    project.addEventListener('mouseover', () => {
+        project.classList.add('hover'); 
+    });
+
+    project.addEventListener('mouseout', () => {
+        project.classList.remove('hover'); 
+    });
+});
